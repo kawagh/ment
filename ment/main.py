ae4205013a04505de28c88d63e4d6c0b9de26d0b
kawagh <base1611ball@gmail.com>
2021-02-19 22:13:23 +0900

    implemented argparse -d

--- ment/main.py
+++ ment/main.py
@@ -1,14 +1,70 @@
+import argparse
 import datetime
 import os
 import subprocess
 
 
+def get_args():
+    """get_args.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument('--synthe')
+    args = parser.parse_args()
+    return args
+
+
+def _tag_parse():
+    pass
+
+
+def synthesize_by_tag(tag, src_dir, dst_dir):
+    """synthesize_by_tag.
+    タグに応じた文書を吸い上げて生成する
+
+    Args:
+        tag:統合するタグ
+        src_dir:統合されるマークダウンを格納したディレクトリ群の親ディレクトリ
+        dst_dir:生成するマークダウンの場所
+    """
+    dst_mkd_file = os.path.join(dst_dir, tag + '.md')
+    with open(dst_mkd_file, 'a') as f:
+        f.write(f'# {tag}\n')
+        # TODO
+        # タグに関わる文書を生成していく
+        days = os.listdir(src_dir)
+        print(days)
+        days.sort()
+        print(days)
+        for day in days:
+            print(day)
+            if day == 'synthe':
+                continue
+            src_mkd_file = os.path.join(src_dir, day, 'diary.md')
+            # TODO 見出しを段落でスプリットして行番号ごとにcat
+            subprocess.run(['cat', src_mkd_file])
+
+
 def main():
+    """main.
+    """
+    args = get_args()
     print("This is ment")
     BASE_DIR = os.path.join(os.path.expanduser('~/ment_dir'))
     if not os.path.exists(BASE_DIR):
         os.mkdir(BASE_DIR)
         print(f'{BASE_DIR=}')
+
+    if args.synthe is not None:
+        tag = args.synthe
+        os.makedirs(os.path.join(BASE_DIR, 'synthe', tag), exist_ok=True)
+        dst_dir = os.path.join(BASE_DIR, 'synthe', tag)
+        # tag search
+        synthesize_by_tag(tag, BASE_DIR, dst_dir)
+        print(args.synthe)
+        print("synthe END")
+        exit()
+
     YYYY_MM_DD = str(datetime.date.today())
 
     # title = input("Title:")
@@ -17,15 +73,17 @@ def main():
     os.makedirs(os.path.join(BASE_DIR, YYYY_MM_DD), exist_ok=True)
     file_path = os.path.join(BASE_DIR, YYYY_MM_DD, file_name)
     # edit
-    start_time = str(datetime.datetime.now().strftime('%H:%M:%S'))
-    with open(file_path, 'a') as f:
-        f.write(f'> <!--started:{start_time}--!>\n')
+    if not args.debug:
+        start_time = str(datetime.datetime.now().strftime('%H:%M:%S'))
+        with open(file_path, 'a') as f:
+            f.write(f'> <!--started:{start_time}--!>\n')
 
     subprocess.run(['vim', file_path])
 
-    finish_time = str(datetime.datetime.now().strftime('%H:%M:%S'))
-    with open(file_path, 'a') as f:
-        f.write(f'> <!--stopped:{finish_time}--!>\n')
+    if not args.debug:
+        finish_time = str(datetime.datetime.now().strftime('%H:%M:%S'))
+        with open(file_path, 'a') as f:
+            f.write(f'> <!--stopped:{finish_time}--!>\n')
 
 
 if __name__ == '__main__':
