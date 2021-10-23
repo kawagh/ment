077fb8f43b764e1a861a868b29fb01b9c86326b5
kawagh <kawagh@example.com>
2021-10-23 15:46:30 +0900

    fixed fstring

--- ment/main.py
+++ ment/main.py
@@ -222,7 +222,7 @@ def synthesize_by_tag(tag: str, src_dir: Path, dst_dir: Path):
     print(f"Extracting tag `{tag}` from")
     with open(output_file, "w") as f:
         for src_mkd_dir in mkd_dir_paths:
-            diary_path = Path(src_mkd_dir) / "f{src_mkd_dir.name}.md"
+            diary_path = Path(src_mkd_dir) / f"{src_mkd_dir.name}.md"
             if not diary_path.exists():
                 diary_path = Path(src_mkd_dir) / "diary.md"
             if diary_path.exists():
